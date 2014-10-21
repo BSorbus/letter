f6f7a27d806a140cafd91ba70352be9307fadefa
BSorbus <BSorbus@gmail.com>
2014-10-21 08:55:01 +0200

    add LinkedIn in the social login provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+      
   validates_presence_of :email
   mount_uploader :image, ImageUploader
   has_many :authorizations
